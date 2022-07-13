241843852c6d03c70b00af424fb295887fa82a74
Damian Delfino <102768719+DamiDelfino@users.noreply.github.com>
2022-07-13 17:39:45 -0300

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Mujer Invicible', power: 'Invicibilidad y creacion de campos magneticos' },
+    { id: 2, name: 'Antorcha Humana', power: 'Control del fuego y vuelo' },
+    { id: 3, name: 'La Mole', power: 'Super fuerza y cuerpo cubierto de piedra'},
+    { id: 4, name: 'Profesor X', power: 'Telequinesis, telepatia y manipulacion mental'},
+    { id: 5, name: 'Ciclope', power: 'Vision laser' },
+    { id: 6, name: 'Iceman', power: 'Control del hielo' },
+    { id: 7, name: 'Bestia', power: 'Fuerza y velocidad sobrehumana' },
+    { id: 8, name: 'Wolverine', power: 'Garras retractiles, regeneracion y fuerza sobrehumana' },
+    { id: 9, name: 'Fénix', power: 'Control de la materia y energia, manipulacion molecular y telequinesis' },
+    { id: 10, name: 'Tormenta', power: 'Manipulacion del cliema y vuelo' }
+];
